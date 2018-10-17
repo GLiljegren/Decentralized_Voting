6bfeae52ce1d2fd64e46bd80024d1bbf857161b1
Gustaf Liljegren <gusli379@student.liu.se>
2018-10-17 20:47:32 +0200

    Basic election solidity script + initialization-tests
    A solidity script which deploys a contract "Election" and within it creates two candidates.
    
    A javascript to test if there are in fact two candidates when the contract is deployed and that they have the correct initial parameters (id, name, number of votes)

---
+++ Truffle/contracts/Election.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.2;
+
+contract Election{
+	// Model a Candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+	// Store Candidate
+
+	// Fetch Candidate
+	mapping(uint => Candidate) public candidates;
+	
+	// Store Candidate Count
+	uint public candidatesCount;
+
+	function Election () public {
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	// Add a candidate
+	function addCandidate (string _name) private {
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
+}
